85c1e65bf05c1f03c6b745765d1980d0b60a65bf
Christopher Giroir <kelsin@valefor.com>
2014-11-02 23:10:00 -0800

    Update 2014-11-03T07:09:58.204Z

--- doc/assets/behavior.js
+++ doc/assets/behavior.js
@@ -47,10 +47,41 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
         "projectPath": "app/app.js",
         "targetPath": "app",
         "pageTitle": "app",
-        "title": "app"
+        "firstHeader": {
+          "type": "heading",
+          "data": {
+            "level": 1,
+            "title": "app.js",
+            "slug": "appjs",
+            "isFileHeader": true
+          },
+          "depth": 1,
+          "children": [
+            {
+              "type": "heading",
+              "data": {
+                "level": 2,
+                "title": "Initializers",
+                "slug": "initializers"
+              },
+              "depth": 2
+            }
+          ]
+        },
+        "title": "app.js"
       },
       "depth": 1,
-      "outline": []
+      "outline": [
+        {
+          "type": "heading",
+          "data": {
+            "level": 2,
+            "title": "Initializers",
+            "slug": "initializers"
+          },
+          "depth": 2
+        }
+      ]
     }, {
       "type": "folder",
       "data": {
