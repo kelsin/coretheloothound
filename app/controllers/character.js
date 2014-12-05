360c67469c6f36214b0cf0a4139d6207e1e2eed6
Christopher Giroir <cgiroir@blizzard.com>
2014-12-05 11:41:35 -0800

    Found icons for links

--- app/controllers/character.js
+++ app/controllers/character.js
@@ -1,15 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+  dashedName: function() {
+    return this.get('name').dasherize();
+  }.property('name'),
+
+  dashedRealm: function() {
+    return this.get('realm').dasherize();
+  }.property('realm'),
+
   armoryUrl: function() {
-    return 'http://us.battle.net/wow/character/' + this.get('realm') + '/' + this.get('name') + '/simple';
-  }.property('name', 'realm'),
+    return 'http://us.battle.net/wow/character/' + this.get('dashedRealm') + '/' + this.get('dashedName') + '/simple';
+  }.property('dashedName', 'dashedRealm'),
 
   robotUrl: function() {
-    return 'http://www.askmrrobot.com/wow/player/us/' + this.get('realm') + '/' + this.get('name');
-  }.property('name', 'realm'),
+    return 'http://www.askmrrobot.com/wow/player/us/' + this.get('dashedRealm') + '/' + this.get('dashedName');
+  }.property('dashedName', 'dashedRealm'),
 
   wowheadUrl: function() {
-    return 'http://www.wowhead.com/list/us-' + this.get('realm') + '-' + this.get('name');
-  }.property('name', 'realm')
+    return 'http://www.wowhead.com/list/us-' + this.get('dashedRealm') + '-' + this.get('dashedName');
+  }.property('dashedName', 'dashedRealm')
 });
