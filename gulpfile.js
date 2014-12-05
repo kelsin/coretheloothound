ad58653c19ca0bbd992911ebc796e494a610db62
Christopher Giroir <cgiroir@blizzard.com>
2014-12-05 11:28:01 -0800

    Adding CNAME

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 var deploy = require('gulp-gh-pages');
+var file = require('gulp-file');
 var exec = require('child_process').exec;
 var gulp = require('gulp');
+var os = require('os');
 
 gulp.task('deploy', function () {
   return gulp.src('./dist/**/*')
+    .pipe(file('CNAME', 'coretheloothound.com' + os.EOL))
     .pipe(deploy());
 });
 
 gulp.task('doc', function(next) {
   exec('node_modules/groc/bin/groc "app/**/*" "README.md"', function(err, stdout, stderr) {
-    next(err);
+    gulp.src([]).pipe(file('CNAME', 'docs.coretheloothound.com' + os.EOL)).pipe(gulp.dest('doc')).on('end', next);
   });
 });
