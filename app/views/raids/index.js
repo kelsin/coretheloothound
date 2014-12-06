9acb80fb1b7f4f008c195b4ae033c7f503ad8d13
Christopher Giroir <kelsin@valefor.com>
2014-12-05 23:30:08 -0800

    Better popovers

--- app/views/raids/index.js
+++ app/views/raids/index.js
@@ -3,6 +3,10 @@ import AfterRender from '../../mixins/after-render';
 
 export default Ember.View.extend(AfterRender, {
   afterRenderEvent: function() {
-    Ember.$('[data-toggle="popover"]').popover({trigger: 'hover'});
+    Ember.$('[data-toggle="popover"]').popover({
+      title: 'Raid Note',
+      placement: 'top',
+      trigger: 'hover'
+    });
   }
 });
