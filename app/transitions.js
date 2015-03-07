b3949655e809d94a6522fea7ea3408fc10af8d20
Christopher Giroir <kelsin@valefor.com>
2015-03-07 08:56:41 -0800

    Fixing the rimraf problem

--- app/transitions.js
+++ app/transitions.js
@@ -1,5 +1,6 @@
 export default function(){
   this.transition(
+    this.fromRoute('raids.index'),
     this.toRoute('account.characters.index'),
     this.use('toLeft'),
     this.reverse('toRight')
