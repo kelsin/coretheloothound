d2173d8f19fe333e854e39481246b79b01d8e086
Christopher Giroir <kelsin@valefor.com>
2015-03-08 15:38:42 -0700

    Fixing loading of account data

--- app/routes/characters/index.js
+++ app/routes/characters/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.modelFor('application').account.characters;
+    return this.modelFor('application').account.get('characters');
   }
 });
