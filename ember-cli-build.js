0219f0a3796b1b60f65d80ada9036b9e8bb2c8ff
Christopher Giroir <cgiroir@blizzard.com>
2016-09-21 08:51:00 -0700

    Brocfile deprecation

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,37 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  // Modernizr
+  app.import('vendor/modernizr.js');
+
+  // Bootstrap
+  app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
+  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', { destDir: 'fonts' });
+  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', { destDir: 'fonts' });
+  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', { destDir: 'fonts' });
+  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', { destDir: 'fonts' });
+
+  // Font-awesome
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.eot', { destDir: 'fonts' });
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.svg', { destDir: 'fonts' });
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.ttf', { destDir: 'fonts' });
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff', { destDir: 'fonts' });
+
+  // D3
+  app.import('bower_components/d3/d3.js');
+
+  // Lodash
+  app.import('bower_components/lodash/lodash.js');
+
+  // Moment
+  app.import('bower_components/moment/moment.js');
+
+  // Pickadate
+  app.import('bower_components/pickadate/lib/picker.js');
+  app.import('bower_components/pickadate/lib/picker.date.js');
+  app.import('bower_components/pickadate/lib/picker.time.js');
+  app.import('bower_components/pickadate/lib/legacy.js');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
