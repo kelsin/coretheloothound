13ccb524bbc341fb55dc85766f2f18317c4eb591
Christopher Giroir <cgiroir@blizzard.com>
2016-09-21 15:20:16 -0700

    Removing ArrayController

--- app/controllers/raids/index.js
+++ app/controllers/raids/index.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
-export default Ember.ArrayController.extend({
-  sortProperties: ['date', 'name'],
-  itemController: 'raid'
+export default Ember.Controller.extend({
+  raidSorting: ['date', 'name'],
+
+  filteredRaids: Ember.computed('model.@each', function() {
+    return this.get('model').filter(function(raid) {
+      return moment(raid.get('date')).add(6, 'hours').isAfter();
+    });
+  }),
+
+  sortedRaids: Ember.computed.sort('filteredRaids', 'raidSorting')
 });
--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,8 +1,9 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
   isNewSerializerAPI: true,
-  keyForAttribute: function(attr, method) {
+  keyForAttribute: function(attr) {
     return Ember.String.underscore(attr);
   }
 });
