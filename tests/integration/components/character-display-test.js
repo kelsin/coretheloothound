1bdf940b1dde853b2af4b6fd010e5a1d8118701d
Christopher Giroir <cgiroir@blizzard.com>
2016-09-25 23:24:23 -0500

    Removing test and making sure note is styled

--- tests/integration/components/character-display-test.js
+++ tests/integration/components/character-display-test.js
@@ -9,16 +9,16 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{character-display}}`);
+  // this.render(hbs`{{character-display}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  // assert.equal(this.$().text().trim(), '');
 
-  // Template block usage:
-  this.render(hbs`
-    {{#character-display}}
-      template block text
-    {{/character-display}}
-  `);
+  // // Template block usage:
+  // this.render(hbs`
+  //   {{#character-display}}
+  //     template block text
+  //   {{/character-display}}
+  // `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  // assert.equal(this.$().text().trim(), 'template block text');
 });
