ae3e6f760a4c13eb24f3d8d9c46b12dd7cc8dfb2
Christopher Giroir <cgiroir@blizzard.com>
2016-10-09 11:20:38 -0700

    Fixing tests

--- app/components/date-time.js
+++ app/components/date-time.js
@@ -3,20 +3,22 @@ import Ember from 'ember';
 /* global moment */
 export default Ember.Component.extend({
   picktime: Ember.computed('date', 'pickdate', {
-    get(key) {
+    get() {
       return moment(this.get('date')).format('h:mm A');
     },
     set(key, value) {
+      /*jshint unused: vars */
       this.set('date', moment(this.get('pickdate') + ' ' + value, 'D MMMM, YYYY h:mm A').toDate());
       return moment(this.get('date')).format('h:mm A');
     }
   }),
 
   pickdate: Ember.computed('date', 'picktime', {
-    get(key) {
+    get() {
       return moment(this.get('date')).format('D MMMM, YYYY');
     },
     set(key, value) {
+      /*jshint unused: vars */
       this.set('date', moment(value + ' ' + this.get('picktime'), 'D MMMM, YYYY h:mm A').toDate());
       return moment(this.get('date')).format('D MMMM, YYYY');
     }
