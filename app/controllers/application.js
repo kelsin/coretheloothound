ca4713d13eeb1ffb7c1ac63a0e36010f9b6c9749
Caitlin <ayashi@valefor.com>
2016-10-16 21:57:56 -0700

    updating copyright year to show current year

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,5 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   session: Ember.inject.service(),
   apikey: Ember.computed.alias('session.apikey'),
-  loggedIn: Ember.computed.alias('session.loggedIn')
+  loggedIn: Ember.computed.alias('session.loggedIn'),
+  currentYear: Ember.computed(function(){
+    return moment().format('YYYY');
+  })
 });
