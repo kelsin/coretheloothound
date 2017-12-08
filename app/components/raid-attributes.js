2e3e2085ef27268adf2b1dbf1e8bc9f65c455f7f
Caitlin <ayashi@valefor.com>
2017-12-07 21:19:13 -0800

    couple changes so raid name is in the field by default and you cannot submit an empty name

--- app/components/raid-attributes.js
+++ app/components/raid-attributes.js
@@ -1,9 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  didInsertElement() {
+    this.set('newRaidName', this.get('raid.name'))
+  },
+  trimmedName: Ember.computed('newRaidName', function(){
+    return this.get('newRaidName').trim()
+  }),
   actions: {
     editRaidName() {
-      this.sendAction('editRaidName', this.get('newRaidName'));
+      if(this.get('trimmedName').length > 0){
+        this.sendAction('editRaidName', this.get('newRaidName'));
+      }
+      
     }
   }
 });
